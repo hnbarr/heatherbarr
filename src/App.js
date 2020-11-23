b0480f99f8a280e22ed965f41b7da219987aa3c6
Heather Barr <hnbarr@yahoo.com>
2020-11-23 16:34:49 -0600

    removing import of navlink

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React,  { useState } from 'react';
-import { BrowserRouter, NavLink } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import Router from './Router'
 import {ThemeProvider} from "styled-components";
 import { GlobalStyles } from "./components/GlobalStyles";
