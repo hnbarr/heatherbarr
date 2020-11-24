a0d436725d948b1c89e7dc24275047e08225a18c
Heather Barr <hnbarr@yahoo.com>
2020-11-24 09:48:53 -0600

    update to contact layout to prep for content

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,8 +3,18 @@ import React, { Component } from 'react'
 export default class Contact extends Component {
     render() {
         return (
-            <div className="container">
-                bio here
+            <div className="container h-100" id="contact">
+                <div className="row d-flex justify-content-center align-items-center h-100">
+                    <div className="col-12">
+                        image
+                    </div>
+                    <div className="col-12">
+                        title
+                    </div>
+                    <div className="col-12">
+                        social links
+                    </div>
+                </div>
             </div>
         )
     }
